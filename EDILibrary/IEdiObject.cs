ffa54c466aaf5e6ac13b1e341cf6631fa7eccb64
Joscha Metze <joscha.metze@hochfrequenz.de>
2021-12-02 11:45:31 +0100

    next stop: formatters

--- EDILibrary/IEdiObject.cs
+++ EDILibrary/IEdiObject.cs
@@ -113,7 +113,7 @@ namespace EDILibrary
                     {
                         if (child.Value.Type == JTokenType.Float)
                         {
-                            Fields.Add(child.Name, new List<string> { child.Value<float>().ToString() });
+                            Fields.Add(child.Name, new List<string> { child.Value.ToString("F3") });
                         }
                         else
                         {
