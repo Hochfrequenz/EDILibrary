a730a2811026d27add87ed97be500f569ff9638f
konstantin <konstantin.klein@hochfrequenz.de>
2022-03-04 13:51:18 +0100

    remove BOMs from all files

--- EDILibrary/GenericEDILoader.cs
+++ EDILibrary/GenericEDILoader.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2017 Hochfrequenz Unternehmensberatung GmbH
+// Copyright (c) 2017 Hochfrequenz Unternehmensberatung GmbH
 using System;
 using System.Collections.Generic;
 using System.IO;
